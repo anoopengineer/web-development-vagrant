f1e5be333cc6a4332beaf419210ca65435e01f76
Anoop Kunjuraman <Anoop.KunjuramanPillai@capitalone.com>
2016-01-18 10:37:37 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Anoop's Web Development Vagrant box ##
+
+Helps you spin up a virtual machine that supports web development in minutes.
+
+Has the following installed:
+1. Node and NPM
+1. Git
+1. Coffee script
+1. Grunt
+1. Yeoman
+1. Yeoman Angular and Webapp Generator
+1. Compass
+
+This vagrant box is based on `ubuntu/trusty32`. Edit Vagrantfile if you want to use a different base image.
---
+++ provision.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "Anoop's Web development box. Starting Provisioning"
+apt-get update
+
+echo "Installing git"
+apt-get install -y git
+
+echo "Installing curl"
+apt-get install -y curl
+
+echo "Installing node and npm"
+curl -sL https://deb.nodesource.com/setup_4.x | bash -
+apt-get install -y nodejs build-essential
+npm install -g npm
+
+echo "Install grunt dependencies"
+npm install -g coffee-script
+npm install -g grunt-cli
+
+echo "Installing Yeoman and generators"
+npm install -g yo
+npm install -g generator-angular
+npm install -g generator-webapp
+
+echo "Installing compass support"
+apt-get install -y ruby-dev
+gem install compass --no-ri --no-rdoc
